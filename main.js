36dbfd4e111399b3a0908eb6995de259963a51bd
tamurayuuiti <152710047+tamurayuuiti@users.noreply.github.com>
2025-06-15 22:42:09 +0900

    Update main.js

--- main.js
+++ main.js
@@ -75,8 +75,7 @@ function createHintInputList(type, size) {
     label.className = 'hint-input-label';
     label.textContent = i + 1;
     const input = document.createElement('input');
-    input.type = 'text';
-    input.inputMode = 'tel';
+    input.type = 'tel';
     input.pattern = '[0-9, ]*'; // スペースも許可
     input.className = 'hint-input-field';
     input.placeholder = '例: 3,1,2';
